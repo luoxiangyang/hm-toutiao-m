69dd92bdd2671a397a00b044da06c9eb59b54054
Programmer-new <2585710570@qq.com>
2020-03-15 14:35:55 +0800

    路由规则

--- src/router/index.js
+++ src/router/index.js
@@ -1,17 +1,55 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import Home from '@/views/home'
 
 Vue.use(VueRouter)
+const Layout = () => import('@/views/layout')
+const home = () => import('@/views/home')
+const question = () => import('@/views/question')
+const video = () => import('@/views/video')
+const user = () => import('@/views/user')
+const chat = () => import('@/views/user/chat')
+const login = () => import('@/views/login')
+const profile = () => import('@/views/user/profile')
+const search = () => import('@/views/search')
+const searchResult = () => import('@/views/search/result')
+const article = () => import('@/views/article')
 
-const routes = [{
-  path: '/',
-  redirect: '/home'
-},
-{
-  path: '/home',
-  component: Home
-}
+const routes = [
+  {
+    path: '/',
+    component: Layout,
+    children: [{
+      path: '', // 默认二级路由的组件
+      component: home
+    }, {
+      path: '/question',
+      component: question
+    }, {
+      path: '/video',
+      component: video
+    }, {
+      path: '/user',
+      component: user
+    }]
+  }, {
+    path: '/user/chat',
+    component: chat
+  }, {
+    path: '/login',
+    component: login
+  }, {
+    path: '/user/profile',
+    component: profile
+  }, {
+    path: '/search',
+    component: search
+  }, {
+    path: '/search/result',
+    component: searchResult
+  }, {
+    path: '/article',
+    component: article
+  }
   // {
   //   component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
   // }
